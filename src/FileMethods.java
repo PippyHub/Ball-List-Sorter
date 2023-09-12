efe5703b615233ef9f4957e0469729c4364afe30
Piper Inns Hall <piper.innshall@gmail.com>
2023-09-13 11:06:30 +1200

    Prints filtered students to output file

--- src/FileMethods.java
+++ src/FileMethods.java
@@ -1,9 +1,9 @@
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-
 public class FileMethods {
     public static String[] fileRead(File file) {
         List<String> linesList = new ArrayList<>();
@@ -19,20 +19,15 @@ public class FileMethods {
         }
         return linesList.toArray(new String[0]);
     }
-    static String[] filterStudents(String[] students, String[] attendees) {
-        List<String> filteredList = new ArrayList<>();
-        for (String student : students) {
-            if (student.startsWith("Ropu")) {
-                filteredList.add(student);
-                continue;
+    public static void fileWrite(String[] string) {
+        try {
+            FileWriter writer = new FileWriter("src/files/Output");
+            for (String item : string) {
+                writer.write(item + "\n");
             }
-            for (String attendee : attendees) {
-                if (attendee.contains(student)) {
-                    filteredList.add(student);
-                    break;
-                }
-            }
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        return filteredList.toArray(new String[0]);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,29 @@
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
-    static String[] Students;
+     static String[] Students;
     static String[] Attendees;
     public static void main(String[] args) {
         Students = FileMethods.fileRead(new File("src/files/Students"));
         Attendees = FileMethods.fileRead(new File("src/files/Attendees"));
-
-        System.out.println("Students:");
-        printArray(Students);
-
-        System.out.println("\nAttendees:");
-        printArray(Attendees);
-
-        Students = FileMethods.filterStudents(Students, Attendees);
-
-        System.out.println("\nFiltered Students:");
-        printArray(Students);
+        FileMethods.fileWrite(filterStudents(Students, Attendees));
     }
-    static void printArray(String[] array) {
-        for (String item : array) {
-            System.out.println(item);
+    static String[] filterStudents(String[] string1, String[] string2) {
+        List<String> filteredList = new ArrayList<>();
+        for (String student : string1) {
+            if (student.startsWith("Ropu")) {
+                filteredList.add(student);
+                continue;
+            }
+            for (String attendee : string2) {
+                if (attendee.contains(student)) {
+                    filteredList.add(student);
+                    break;
+                }
+            }
         }
+        return filteredList.toArray(new String[0]);
     }
 }
